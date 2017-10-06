d2c518c6451a629a66bb41fad4f3e8398fc8b02f
MARTIN Pierre <HicksCorp@GMail.com>
2017-10-06 17:24:05 +0100

    Reverts some comsmetic changes that might have introduced a bug, performs some cosmetic and readability cleanups.

--- bin/publish.sh
+++ bin/publish.sh
@@ -12,10 +12,12 @@ fi
 
 echo "Stamping git repo with tag $VERSION..."
 sed -i "s/Stable tag: .*/Stable tag: $VERSION/g" readme.txt
+sed -i 's/  "version": ".*",/  "version": "'$VERSION'",/g' package.json
 sed -i "s/const VERSION = '.*';/const VERSION = '$VERSION';/g" snappic-for-woocommerce.php
 sed -i 's/"Project-Id-Version: Snappic for WooCommerce .*\\n"/"Project-Id-Version: Snappic for WooCommerce '$VERSION'\\n"/g' languages/snappic-for-woocommerce.pot
-git add readme.txt snappic-for-woocommerce.php languages/snappic-for-woocommerce.pot
+git add readme.txt package.json snappic-for-woocommerce.php languages/snappic-for-woocommerce.pot
 git commit -m "Bumps version to $VERSION and tags."
+git tag $VERSION
 git push origin master --tags
 
 echo "Removing previous SVN repository versions..."
--- includes/class-snappic-helper.php
+++ includes/class-snappic-helper.php
@@ -1,9 +1,8 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 class Snappic_Helper {
-
     const TESTPIXEL = '123123123';
     const API_HOST_DEFAULT = 'https://api.snappic.io';
     const API_SANDBOX_HOST_DEFAULT = 'http://api.magento-sandbox.snappic.io';
@@ -20,45 +19,41 @@ class Snappic_Helper {
     * @since  1.0.0
     */
     public static function get_instance() {
-        if ( is_null( self::$_instance ) )
+        if (is_null(self::$_instance))
             self::$_instance = new self();
         return self::$_instance;
     }
-
 
     /**
     * Fetch the pixel from API
     * @todo : add timer (don't do this every page load)
     * @since  1.0.0
     */
-    public function fetch_pixel(){
+    public function fetch_pixel () {
 
-        $result = wp_cache_get( 'snappic_pixel_request', 'api_calls' );
+        $result = wp_cache_get('snappic_pixel_request', 'api_calls');
 
-        if ( false === $result ) {
-            $result = wp_safe_remote_get( $this->get_api_url() );
-            wp_cache_set( 'snappic_pixel_request', $result, 'api_calls', 900 );  // cache for 15 minutes
+        if (false === $result) {
+            $result = wp_safe_remote_get($this->get_api_url());
+            wp_cache_set('snappic_pixel_request', $result, 'api_calls', 900);
         }
 
         // Do something with $result
-        if( is_wp_error( $result ) ) {
-            return false; // Bail early
+        if (is_wp_error($result)) {
+            return false;
         }
 
-        $body = wp_remote_retrieve_body( $result );
-        $data = json_decode( $body );
-
-        // If Facebook pixel is not empty string, we need to save it.
-        if( isset( $data->facebook_pixel_id ) && '' !== $data->facebook_pixel_id ) {
-            // If the pixel was saved, delete the cached API call
-            if( $this->save_pixel_id( $data->facebook_pixel_id ) ) {
-                wp_cache_delete( 'snappic_pixel_request', 'api_calls' );
-            }
+        $body = wp_remote_retrieve_body($result);
+        $data = json_decode($body);
+        // No pixel...
+        if (!isset($data->facebook_pixel_id) || $data->facebook_pixel_id == '') {
+            return false;
         }
-
-
+        // If pixel is valid and safe, clear the cache pixel result.
+        if ($this->save_pixel_id($data->facebook_pixel_id)) {
+            wp_cache_delete('snappic_pixel_request', 'api_calls');
+        }
     }
-
 
     /**
     * Get the pixel ID that is stored in the admin settings
@@ -66,9 +61,9 @@ class Snappic_Helper {
     * @since  1.0.0
     */
     public function get_stored_pixel_id() {
-        if( ! $this->stored_pixel_id ) {
+        if (!$this->stored_pixel_id) {
             $settings = Snappic_Integration::instance();
-            $this->stored_pixel_id = $settings->get_option( 'pixel_id' );
+            $this->stored_pixel_id = $settings->get_option('pixel_id');
         }
         return $this->stored_pixel_id;
     }
@@ -79,7 +74,7 @@ class Snappic_Helper {
     * @since  1.0.0
     */
     public function get_pixel_id() {
-        if( ! $this->pixel_id ) {
+        if (!$this->pixel_id) {
             $this->pixel_id = $this->is_sandboxed() ? self::TESTPIXEL : $this->get_stored_pixel_id();
         }
         return $this->pixel_id;
@@ -90,11 +85,9 @@ class Snappic_Helper {
     * @return bool
     * @since  1.0.0
     */
-    public function save_pixel_id( $pixel_id ) {
-        $new_options = array( 'pixel_id' => $pixel_id );
-        return $this->update_options( $new_options );
+    public function save_pixel_id($pixel_id) {
+        return $this->update_options(array('pixel_id' => $pixel_id));
     }
-
 
     /**
     * Save an array of options
@@ -103,39 +96,33 @@ class Snappic_Helper {
     * @return array
     * @since  1.0.0
     */
-    public function update_options( $options = array() ) {
+    public function update_options($options = array()) {
         $settings = Snappic_Integration::instance();
-        $old_options = (array) get_option( $settings->get_option_key() );
-        $new_options = apply_filters( 'woocommerce_settings_api_sanitized_fields_' . $settings->id, $options );
-        $updated_options = array_merge( $old_options, $new_options );
-
-        return update_option( $settings->get_option_key(), $updated_options );
+        $old_options = (array) get_option($settings->get_option_key());
+        $new_options = apply_filters('woocommerce_settings_api_sanitized_fields_' . $settings->id, $options);
+        $updated_options = array_merge($old_options, $new_options);
+        return update_option($settings->get_option_key(), $updated_options);
     }
-
 
     /**
     * Determine if we need to fetch the pixel from API
     * @since  1.0.0
     */
     public function needs_pixel() {
-
         $pixel_id = $this->get_stored_pixel_id();
-
-        if( ( $this->is_sandboxed() && '' == $pixel_id ) || ( $this->is_live() && in_array( $pixel_id, array( '', self::TESTPIXEL ) ) ) ) {
+        if (($this->is_sandboxed() && '' == $pixel_id) || ($this->is_live() && in_array($pixel_id, array('', self::TESTPIXEL)))) {
             return true;
         } else {
             return false;
         }
-
     }
-
 
     /**
     * Determine if we have a pixel
     * @since  1.0.0
     */
     public function has_pixel() {
-        return (bool) ! $this->needs_pixel();
+        return (bool)!$this->needs_pixel();
     }
 
     /**
@@ -144,9 +131,8 @@ class Snappic_Helper {
      * @return bool
      */
     public function is_live() {
-        return (bool) ! $this->is_sandboxed();
+        return (bool)!$this->is_sandboxed();
     }
-
 
     /**
      * Get whether or not sandbox mode is enabled
@@ -154,7 +140,7 @@ class Snappic_Helper {
      * @return bool
      */
     public function is_sandboxed() {
-        return (bool) ( 'sandbox' == $this->get_mode() );
+        return (bool) ('sandbox' == $this->get_mode());
     }
 
     /**
@@ -164,7 +150,7 @@ class Snappic_Helper {
     */
     public function get_mode() {
         $settings = Snappic_Integration::instance();
-        return $settings->get_option( 'mode' );
+        return $settings->get_option('mode');
     }
 
     /**
@@ -173,11 +159,11 @@ class Snappic_Helper {
      * @param  bool $bypassSandbox
      * @return string
      */
-    public function get_api_host( $bypassSandbox = false ) {
-        if ( ! $bypassSandbox && $this->is_sandboxed() ) {
+    public function get_api_host($bypassSandbox = false) {
+        if (!$bypassSandbox && $this->is_sandboxed()) {
             return self::API_SANDBOX_HOST_DEFAULT;
         }
-        return $this->getEnvOrDefault( 'SNAPPIC_API_HOST', self::API_HOST_DEFAULT );
+        return $this->getEnvOrDefault('SNAPPIC_API_HOST', self::API_HOST_DEFAULT);
     }
 
     /**
@@ -186,8 +172,8 @@ class Snappic_Helper {
      * @param  bool $bypassSandbox
      * @return string
      */
-    public function get_api_url( $bypassSandbox = false ) {
-        return add_query_arg( 'domain', $this->get_site_domain(), $this->get_api_host( $bypassSandbox ) . '/stores/current' );
+    public function get_api_url($bypassSandbox = false) {
+        return add_query_arg('domain', $this->get_site_domain(), $this->get_api_host($bypassSandbox) . '/stores/current');
     }
 
     /**
@@ -197,15 +183,21 @@ class Snappic_Helper {
      * @param  bool $bypassSandbox
      * @return string
      */
-    public function get_checkout_tracker_url( $order_id, $bypassSandbox = false ) {
-        return add_query_arg( array( 'store_domain' => $this->get_site_domain(), 'order_id' => $order_id ), $this->get_api_host( $bypassSandbox ) . '/checkout_trackers/record' );
+    public function get_checkout_tracker_url($order_id, $bypassSandbox = false) {
+        return add_query_arg(
+            array(
+                'store_domain' => $this->get_site_domain(),
+                'order_id' => $order_id
+            ),
+            $this->get_api_host($bypassSandbox).'/checkout_trackers/record'
+        );
     }
 
     /**
      * @return string
      */
     public function get_snappic_admin_url() {
-        return $this->getEnvOrDefault( 'SNAPPIC_ADMIN_URL', self::SNAPPIC_ADMIN_URL_DEFAULT);
+        return $this->getEnvOrDefault('SNAPPIC_ADMIN_URL', self::SNAPPIC_ADMIN_URL_DEFAULT);
     }
 
     /**
@@ -214,40 +206,44 @@ class Snappic_Helper {
      * @param  str $plan starter|growth
      * @return string
      */
-    public function get_signup_url( $plan = '' ) {
-
+    public function get_signup_url($plan = '') {
         $settings = Snappic_Integration::instance();
-
-        $consumerKey = $settings->get_option( 'cust_key');
-        $consumerSecret = $settings->get_option( 'cust_secret' );
-
+        $consumerKey = $settings->get_option('cust_key');
+        $consumerSecret = $settings->get_option('cust_secret');
         $query_args = array(
             'login' => true,
             'provider'  => 'woocommerce',
             'domain' => urlencode($this->get_site_domain()),
             'access_token' => urlencode($consumerKey.':'.$consumerSecret)
-        );
+       );
 
-        // Validate the plan is either starter or growth.
-        $plan = in_array( $plan, array( 'starter', 'growth' ) ) ? $plan : '';
+        $plan = in_array($plan, array('starter', 'growth')) ? $plan : '';
+        if ($plan) {
+            $query_args['sra_plan'] = $plan;
+        }
 
-        if ($plan) { $query_args['sra_plan'] = $plan; }
-
-        return add_query_arg( $query_args, $this->get_snappic_admin_url() );
+        return add_query_arg($query_args, $this->get_snappic_admin_url());
     }
 
+    /**
+     * Return the checkout tracker url for the Snappic API
+     *
+     * @return string
+     */
+    public function get_login_url() {
+        return add_query_arg('login', '', $this->get_snappic_admin_url());
+    }
 
     /**
      * Parse the site's domain
      *
      * @return string
      */
-    public function get_site_domain(){
+    public function get_site_domain () {
         $domain = get_site_url();
-        $components = parse_url( $domain );
-        return strtolower( $components['host'] );
+        $components = parse_url($domain);
+        return strtolower($components['host']);
     }
-
 
     /**
      * Return from environment variables or a default value
@@ -256,9 +252,9 @@ class Snappic_Helper {
      * @param  string $key
      * @return string
      */
-    public function getEnvOrDefault( $key, $default = null ) {
-        $val = getenv( $key );
-        return empty( $val ) ? $default : $val;
+    public function getEnvOrDefault($key, $default = null) {
+        $val = getenv($key);
+        return empty($val) ? $default : $val;
     }
 
 }
